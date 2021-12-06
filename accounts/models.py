b0b90fa33800b3f7340c7af62c6b751d29a79340
Yona-Dav <yona.bohbot@gmail.com>
2021-12-07 00:15:21 +0200

    update image profile

--- accounts/models.py
+++ accounts/models.py
@@ -14,7 +14,7 @@ class TypeProfile(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     type = models.ForeignKey(TypeProfile, to_field='name', default='beginner',on_delete=models.CASCADE,)
-    image = models.ImageField(upload_to='profile_pic/', default='https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg', blank=True, null=True)
+    image = models.ImageField(upload_to='profile_pic/', default='profile_pic/profile_picture.jpg')
     number_points = models.IntegerField(default=100)
 
     def pic(self):
